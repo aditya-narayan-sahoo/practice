f6508be4e43304f041e1976cf89d15a150096b29
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-09-30 13:11:09 +0530

    Add todo and subTodo model

---
+++ models/todos/todo.models.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const todoSchema = new mongoose.Schema(
+  {
+    content: { type: String, required: true },
+    complete: { type: Boolean, default: false },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    subTodos: [{ type: mongoose.Schema.Types.ObjectId, ref: "SubTodos" }], //sub todos array
+  },
+  { timestamps: true }
+);
+export const Todo = mongoose.model("Todo", todoSchema);
